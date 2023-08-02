24bc4d4e391c178bc7d7df12923ee82d12d58bdf
Jerome Desouches <jerome.desouches@gmail.com>
2023-08-02 12:59:56 +0200

    section editeur crée jusqu'à ombre - sans fonctions pour modifier les textarea

--- frontend/src/components/EditorTextStyle.jsx
+++ frontend/src/components/EditorTextStyle.jsx
@@ -2,57 +2,215 @@ import positionGauche from "../assets/images/positionGauche.png"
 import positionCentre from "../assets/images/positionCentre.png"
 import positionDroite from "../assets/images/positionDroite.png"
 import positionJustify from "../assets/images/positionJustify.png"
-// import ombre from "../assets/images/ombre.png"
-// import bordureRadius from "../assets/images/bordureRadius.png"
-// import bordureOn from "../assets/images/bordureOn.png"
-// import bordureOff from "../assets/images/bordureOff.png"
-import { useState } from "react"
+import ombre from "../assets/images/ombre.png"
+import ombreOff from "../assets/images/ombreOff.png"
+import bordureRadius from "../assets/images/bordureRadius.png"
+import bordureOn from "../assets/images/bordureOn.png"
+import bordureOff from "../assets/images/bordureOff.png"
+import { SketchPicker } from "react-color"
+import { React, useState } from "react"
 
 export default function EditorTextStyle() {
+  const [coordX, setCoordX] = useState(0)
+  const [coordY, setCoordY] = useState(0)
+  const [itemWidth, setItemWidth] = useState(0)
+  const [itemHeight, setItemHeight] = useState(0)
   const [font, setFont] = useState("Serif")
+  const [pickerTextColor, setPickerTextColor] = useState("#000000")
+  const [textColor, setTextColor] = useState("#000000")
+  const [textColorVisible, setTextColorVisible] = useState(false)
+  const [borderThickness, setBorderThickness] = useState(1)
+  const [pickerBorderColor, setPickerBorderColor] = useState("#000000")
+  const [borderColor, setBorderColor] = useState("#000000")
+  const [borderColorVisible, setBorderColorVisible] = useState(false)
+  const [divBorderRadius, setDivBorderRadius] = useState(0)
+  const [ombreX, setOmbreX] = useState(0)
+  const [ombreY, setOmbreY] = useState(0)
+  const [ombreAlpha, setOmbreAlpha] = useState(0)
+  const [ombreBeta, setOmbreBeta] = useState(0)
+  const [pickerShadowColor, setPickerShadowColor] = useState("#000000")
+  const [shadowColor, setShadowColor] = useState("#000000")
+  const [shadowColorVisible, setShadowColorVisible] = useState(false)
+
+  const handleChangeCoordX = (e) => {
+    setCoordX(e.target.value)
+  }
+
+  const handleChangeCoordY = (e) => {
+    setCoordY(e.target.value)
+  }
+
+  const handleChangeItemWidth = (e) => {
+    setItemWidth(e.target.value)
+  }
+
+  const handleChangeItemHeight = (e) => {
+    setItemHeight(e.target.value)
+  }
 
   const handleChangeFont = (e) => {
     setFont(e.target.value)
+  }
+
+  const handleClickDivTextColor = () => {
+    setTextColorVisible(true)
+  }
+
+  const handleChangeColorText = (color) => {
+    setPickerTextColor(color.rgb)
+    setTextColor(
+      `rgba(${color.rgb.r},${color.rgb.g},${color.rgb.b},${color.rgb.a})`
+    )
+  }
+
+  const handleLeaveTextColor = () => {
+    setTextColorVisible(false)
+  }
+
+  const handleChangeBorderThickness = (e) => {
+    setBorderThickness(e.target.value)
+  }
+
+  const handleClickDivBorderColor = () => {
+    setBorderColorVisible(true)
+  }
+
+  const handleChangeColorBorder = (color) => {
+    setPickerBorderColor(color.rgb)
+    setBorderColor(
+      `rgba(${color.rgb.r},${color.rgb.g},${color.rgb.b},${color.rgb.a})`
+    )
+  }
+
+  const handleLeaveBorderColor = () => {
+    setBorderColorVisible(false)
+  }
+
+  const handleChangeDivBorderRadius = (e) => {
+    setDivBorderRadius(e.target.value)
+  }
+
+  const handleChangeOmbreX = (e) => {
+    setOmbreX(e.target.value)
+  }
+
+  const handleChangeOmbreY = (e) => {
+    setOmbreY(e.target.value)
+  }
+
+  const handleChangeOmbreAlpha = (e) => {
+    setOmbreAlpha(e.target.value)
+  }
+
+  const handleChangeOmbreBeta = (e) => {
+    setOmbreBeta(e.target.value)
+  }
+
+  const handleClickDivShadowColor = () => {
+    setShadowColorVisible(true)
+  }
+
+  const handleChangeColorShadow = (color) => {
+    setPickerShadowColor(color.rgb)
+    setShadowColor(
+      `rgba(${color.rgb.r},${color.rgb.g},${color.rgb.b},${color.rgb.a})`
+    )
+  }
+
+  const handleLeaveShadowColor = () => {
+    setShadowColorVisible(false)
   }
 
   return (
     <main className="main-editorTextStyle">
       <p>Position du composant</p>
       <div className="positions-composant">
-        <img src={positionGauche} alt="position gauche" />
-        <img src={positionCentre} alt="position centre" />
-        <img src={positionDroite} alt="position droite" />
+        <img
+          src={positionGauche}
+          alt="position gauche"
+          title="Positionner l'élément à gauche"
+        />
+        <img
+          src={positionCentre}
+          alt="position centre"
+          title="Centrer l'élément dans la page"
+        />
+        <img
+          src={positionDroite}
+          alt="position droite"
+          title="Positionner l'élément à droite"
+        />
       </div>
 
       <section className="positions-detaillees">
         <div className="positions-xy">
           <div className="positionX">
             <p>X :</p>
-            <input type="number" min={0} max={100} />
+            <input
+              type="number"
+              min={0}
+              max={100}
+              value={coordX}
+              onChange={handleChangeCoordX}
+            />
           </div>
           <div className="positionY">
             <p>Y :</p>
-            <input type="number" min={0} max={100} />
+            <input
+              type="number"
+              min={0}
+              max={100}
+              value={coordY}
+              onChange={handleChangeCoordY}
+            />
           </div>
         </div>
         <div className="dimensions-wh">
           <div className="dimensionW">
             <p>W :</p>
-            <input type="number" min={0} max={100} />
+            <input
+              type="number"
+              min={0}
+              max={100}
+              value={itemWidth}
+              onChange={handleChangeItemWidth}
+            />
           </div>
           <div className="dimensionH">
             <p>H :</p>
-            <input type="number" min={0} max={100} />
+            <input
+              type="number"
+              min={0}
+              max={100}
+              value={itemHeight}
+              onChange={handleChangeItemHeight}
+            />
           </div>
         </div>
       </section>
 
       <p>Texte</p>
       <div className="positions-composant">
-        <img src={positionGauche} alt="alignement gauche" />
-        <img src={positionCentre} alt="alignement centre" />
-        <img src={positionDroite} alt="alignement droite" />
-        <img src={positionJustify} alt="alignement justifié" />
+        <img
+          src={positionGauche}
+          alt="alignement gauche"
+          title="Aligner le texte à gauche"
+        />
+        <img
+          src={positionCentre}
+          alt="alignement centre"
+          title="Centrer le texte"
+        />
+        <img
+          src={positionDroite}
+          alt="alignement droite"
+          title="Aligner le texte à droite"
+        />
+        <img
+          src={positionJustify}
+          alt="alignement justifié"
+          title="Justifier le texte"
+        />
       </div>
 
       <section className="police-couleur-texte">
@@ -120,21 +278,156 @@ export default function EditorTextStyle() {
           </option>
         </select>
 
-        {/* font-family:"Arial";
-        font-family: "Cambria";
-        font-family: 'Courier New';
-        font-family: cursive;
-        font-family: fantasy;
-        font-family: 'Franklin Gothic Medium';
-        font-family: "Georgia";
-        font-family: 'Gill Sans MT';
-        font-family: "Impact";
-        font-family: 'Lucida Sans';
-        font-family: monospace;
-        font-family: 'Segoe UI';
-        font-family: "Times New Roman";
-        font-family: 'Trebuchet MS';
-        font-family: "Verdana"; */}
+        <div className="text-color" onClick={handleClickDivTextColor}>
+          <div className="choix-color">
+            <p>A</p>
+            <div style={{ backgroundColor: textColor }}></div>
+          </div>
+          <div className="choice-arrow"></div>
+          {textColorVisible && (
+            <div
+              className="textColorPicker-container"
+              onMouseLeave={handleLeaveTextColor}
+            >
+              <SketchPicker
+                color={pickerTextColor}
+                onChangeComplete={handleChangeColorText}
+                width="300px"
+              />
+            </div>
+          )}
+        </div>
+      </section>
+
+      <p>Bordures</p>
+      <section className="section-bordures">
+        <div className="section-bordures-ligne1">
+          <img
+            src={bordureOn}
+            alt="bordure on"
+            title="Ajouter une bordure à l'élément"
+          />
+          <img
+            src={bordureOff}
+            alt="bordure off"
+            title="Supprimer la bordure"
+          />
+          <label htmlFor="borderThickness">ep :</label>
+          <input
+            type="number"
+            id="borderThickness"
+            min={1}
+            max={15}
+            value={borderThickness}
+            onChange={handleChangeBorderThickness}
+          />
+          <div className="text-color" onClick={handleClickDivBorderColor}>
+            <div className="choix-color">
+              <img className="imgChoixColor" src={bordureOn} alt="bordure on" />
+              <div style={{ backgroundColor: borderColor }}></div>
+            </div>
+            <div className="choice-arrow"></div>
+            {borderColorVisible && (
+              <div
+                className="textColorPicker-container"
+                onMouseLeave={handleLeaveBorderColor}
+              >
+                <SketchPicker
+                  color={pickerBorderColor}
+                  onChangeComplete={handleChangeColorBorder}
+                  width="300px"
+                />
+              </div>
+            )}
+          </div>
+        </div>
+        <div className="section-bordures-ligne2">
+          <img
+            src={bordureRadius}
+            alt="bordure radius"
+            title="Entrez à côté un rayon pour les bordures"
+          />
+          <input
+            type="number"
+            min={0}
+            max={500}
+            value={divBorderRadius}
+            onChange={handleChangeDivBorderRadius}
+          />
+        </div>
+      </section>
+
+      <p>Ombre</p>
+      <section className="section-ombre">
+        <div className="choix-ombre">
+          <div className="images-ombre">
+            <img
+              src={ombre}
+              alt="presence ombre"
+              title="Ajouter une ombre à l'élément"
+            />
+            <img src={ombreOff} alt="pas d'ombre" title="Supprimer l'ombre" />
+          </div>
+
+          <div className="text-color" onClick={handleClickDivShadowColor}>
+            <div className="choix-color">
+              <img className="imgChoixColor" src={ombre} alt="couleur ombre" />
+              <div style={{ backgroundColor: shadowColor }}></div>
+            </div>
+            <div className="choice-arrow"></div>
+            {shadowColorVisible && (
+              <div
+                className="textColorPicker-container"
+                onMouseLeave={handleLeaveShadowColor}
+              >
+                <SketchPicker
+                  color={pickerShadowColor}
+                  onChangeComplete={handleChangeColorShadow}
+                  width="300px"
+                />
+              </div>
+            )}
+          </div>
+        </div>
+
+        <div className="shadow-values">
+          <label htmlFor="ombreX">x :</label>
+          <input
+            type="number"
+            id="ombreX"
+            min={0}
+            max={50}
+            value={ombreX}
+            onChange={handleChangeOmbreX}
+          />
+          <label htmlFor="ombreY">y :</label>
+          <input
+            type="number"
+            id="ombreY"
+            min={0}
+            max={50}
+            value={ombreY}
+            onChange={handleChangeOmbreY}
+          />
+          <label htmlFor="ombreAlpha">&#945; :</label>
+          <input
+            type="number"
+            id="ombreAlpha"
+            min={0}
+            max={50}
+            value={ombreAlpha}
+            onChange={handleChangeOmbreAlpha}
+          />
+          <label htmlFor="ombreBeta">&#946; :</label>
+          <input
+            type="number"
+            id="ombreBeta"
+            min={0}
+            max={50}
+            value={ombreBeta}
+            onChange={handleChangeOmbreBeta}
+          />
+        </div>
       </section>
     </main>
   )
