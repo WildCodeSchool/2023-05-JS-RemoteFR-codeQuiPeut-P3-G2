14febbbe6db8a3fb228da05ff43fd65366141cd4
Vivien Floureck <vivienfloureck@outlook.com>
2023-08-25 09:30:59 +0200

    avancement visuel page scripts

--- frontend/src/pages/Scripts.jsx
+++ frontend/src/pages/Scripts.jsx
@@ -1,14 +1,48 @@
 import Button from "../components/Button"
+import Navbar from "../components/Navbar"
 import Switch from "../components/Switch"
+import "./Scripts.scss"
 
 export default function Scripts() {
   return (
-    <>
-      <main>"Page scripts"</main>
+    <div className="container">
+      <Navbar />
+      <header className="Title">
+        <h1 className="Scripts">Scripts-Scripts-Scripts-Scripts</h1>
+      </header>
       <div className="Filter">
-        <Switch />
-        <Button />
+        <div className="Type">
+          <p>One Shot</p>
+          <Switch />
+          <p>Campagne</p>
+        </div>
+        <div className="Button">
+          <Button />
+          <Button />
+          <Button />
+          <Button />
+        </div>
+        <div className="conseiller">
+          <p>The news</p>
+          <p>Most populare</p>
+          <p>All scénarios</p>
+        </div>
+        <div className="univers">
+          <p>UNIVERSE</p>
+        </div>
+        <div className="auteur">
+          <p>Autor</p>
+          <select></select>
+        </div>
+        <div className="Difficultes">
+          <p>Difficulty</p>
+          <select></select>
+        </div>
+        <div className="nombre">
+          <p>Number of player</p>
+          <select></select>
+        </div>
       </div>
-    </>
+    </div>
   )
 }
