e6888048f84f9a06c5a6880c646afae51b168c16
Jerome Desouches <jerome.desouches@gmail.com>
2023-09-13 11:46:49 +0200

    debut forum

--- frontend/src/pages/Forum.jsx
+++ frontend/src/pages/Forum.jsx
@@ -1,9 +1,30 @@
 import Navbar from "../components/Navbar"
+import plusDansRond from "../assets/images/plusDansRond.png"
 
 export default function Forum() {
+  const handleClickAddNewTopic = () => {}
+
   return (
-    <main>
+    <>
       <Navbar />
-    </main>
+      <main className="main-forum">
+        <section className="forum-bandeauGauche">
+          <div className="div-newTopic">
+            <h1>NEW TOPIC</h1>
+            <img
+              src={plusDansRond}
+              alt="Button add new Topic"
+              onClick={handleClickAddNewTopic}
+            />
+          </div>
+
+          <div className="div-categories">
+            <h1>CATEGORIES</h1>
+          </div>
+        </section>
+
+        <section className="forum-bandeauDroite"></section>
+      </main>
+    </>
   )
 }
