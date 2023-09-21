6adc2e4fbcea686e0e82009a2b4b08a0f28acb75
DcDpnT <dupont.d33720@gmail.com>
2023-09-21 17:23:03 +0200

    footer, animation et changement curseur

--- frontend/src/pages/Welcome.jsx
+++ frontend/src/pages/Welcome.jsx
@@ -10,6 +10,7 @@ import axios from "axios"
 import { useState, useEffect, useContext } from "react"
 import CardCampaign from "../components/CardCampaign"
 import MyContext from "../components/MyContext"
+import Footer from "../components/Footer"
 
 const Welcome = () => {
   const [campagnes, setCampagnes] = useState([])
@@ -116,6 +117,7 @@ const Welcome = () => {
             </Slider>
           </div>
         </div>
+        <Footer />
       </main>
     </>
   )
